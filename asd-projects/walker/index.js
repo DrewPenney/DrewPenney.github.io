85146affa867168e894c5f159b0750692bebc91f
Drew Penney <penneydrew304@gmail.com>
2025-02-06 13:44:00 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,15 +10,23 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+ 
+  $("#board").height(600)
+  $("#board").width(600)
+
   var WALKER = {
     XCOORDINATE: 5, YCOORDINATE: 5, XSPEED: 5, YSPEED: 5,
   };
 
+  var WALKER2 = {
+    XCOORDINATE: 5, YCOORDINATE: 5, XSPEED: 5, YSPEED: 5,
+  }
+
   // Game Item Objects
 var KEY = {
-  left: 37, right: 39, top: 40, bottom: 38,
+  left: 37, right: 39, top: 40, bottom: 38, A: 65, D: 68, W: 87, S: 83, 
 };
+
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
@@ -49,8 +57,9 @@ var KEY = {
       WALKER.YSPEED = 5;
     }  if (event.which === KEY.bottom){
       WALKER.YSPEED = -5;
-    }  
 }
+  }
+
 console.log();
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -72,8 +81,18 @@ function handleKeyUp(){
   WALKER.YSPEED = 0
 }
 function wallCollision(){
-$("#board").height(600)
-$("#board").width(600)
+if (WALKER.XCOORDINATE < 0){
+  WALKER.XCOORDINATE = 0
+} 
+if (WALKER.XCOORDINATE > 550){
+  WALKER.XCOORDINATE = 550
+} 
+if (WALKER.YCOORDINATE < 0){
+  WALKER.YCOORDINATE = 0
+}
+if (WALKER.YCOORDINATE > 550){
+  WALKER.YCOORDINATE = 550
+}
 }
   function endGame() {
     // stop the interval timer
